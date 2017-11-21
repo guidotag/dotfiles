ddd49543b5cc997add5d615756cfdd068bc622f0
guidotag <guido.tag@gmail.com>
2017-11-21 20:06:27 -0300

    Add .inputrc

--- .bashrc
+++ .bashrc
@@ -13,9 +13,6 @@ for file in ~/.{bash_prompt,exports}; do
 done;
 unset file;
 
-# Case-insensitive globbing (used in pathname expansion)
-shopt -s nocaseglob;
-
 # Append to the Bash history file, rather than overwriting it
 shopt -s histappend;
 
--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-DIR="$(cd $(dirname ${BASH_SOURCE}) && pwd)"
-
 rsync -avh --no-perms .bash_profile ~
 rsync -avh --no-perms .bash_prompt ~
 rsync -avh --no-perms .bashrc ~
 rsync -avh --no-perms .exports ~
 rsync -avhr --no-perms --exclude ".git" .vim ~
 rsync -avh --no-perms .vimrc ~
+rsync -avh --no-perms .inputrc ~
 
 source ~/.bashrc
