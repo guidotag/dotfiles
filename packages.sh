180ba3767725def80ae4006e959442ecbcd079a5
guidotag <guido.tag@gmail.com>
2017-11-22 01:42:34 -0300

    Package installation

---
+++ packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install apps using apt
+
+# Update packages version information
+sudo apt update
+
+# Upgrade packages
+sudo apt upgrade
+
+# Latex
+sudo apt install texlive-full
+sudo apt install texmaker
+
+# Sublime
+
+# gcc, g++
+
+# Python
+
+# Java
+
+# Inkscape
+
+# Firefox
+
+# Git
+# TODO set up SSH keys
