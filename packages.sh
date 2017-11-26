03d0c997f9696fa05dc54b18ff10a5726fff9318
guidotag <guido.tag@gmail.com>
2017-11-26 17:25:15 +0000

    Add packages

--- packages.sh
+++ packages.sh
@@ -8,21 +8,29 @@ sudo apt update
 # Upgrade packages
 sudo apt upgrade
 
+# Firefox
+sudo apt install firefox
+
+# Git
+sudo apt install git
+
 # Latex
 sudo apt install texlive-full
 sudo apt install texmaker
 
 # Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - # install GPG key
+sudo apt install apt-transport-https # make apt work with https resources
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list # select stable channel
+sudo apt install sublime-text
 
-# gcc, g++
-
-# Python
+# gcc, g++, libc, make, etc.
+sudo apt install build-essential
 
 # Java
+sudo apt install default-jre
+sudo apt install default-jdk
 
 # Inkscape
+sudo apt install inkscape
 
-# Firefox
-
-# Git
-# TODO set up SSH keys
