ee41fd01e7b7ae541094afe7cdc2515d77bfb9f5
guidotag <guido.tag@gmail.com>
2018-01-24 14:38:26 -0500

    Add apparently necessary apt update

--- packages.sh
+++ packages.sh
@@ -24,6 +24,7 @@ sudo apt install texmaker
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - # install GPG key
 sudo apt install apt-transport-https # make apt work with https resources
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list # stable version
+sudo apt update
 sudo apt install sublime-text
 
 # gcc, g++, libc, make, etc.
@@ -35,4 +36,3 @@ sudo apt install default-jdk
 
 # Inkscape
 sudo apt install inkscape
-
